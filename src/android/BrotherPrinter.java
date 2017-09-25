7d085742070364d9190cc83b1f3257f7c453d0e8
damian sastre <tagerr@gmail.com>
2017-09-25 17:07:09 -0300

    Update BrotherPrinter.java

--- src/android/BrotherPrinter.java
+++ src/android/BrotherPrinter.java
@@ -199,7 +199,7 @@ public class BrotherPrinter extends CordovaPlugin {
                     myPrinterInfo.port          = PrinterInfo.Port.NET;
                     myPrinterInfo.printMode     = PrinterInfo.PrintMode.ORIGINAL;
                     myPrinterInfo.orientation   = PrinterInfo.Orientation.LANDSCAPE;
-                    myPrinterInfo.paperSize     = PrinterInfo.PaperSize.CUSTOM;
+                    myPrinterInfo.paperSize     = PrinterInfo.PaperSize.A3;
                     myPrinterInfo.ipAddress     = ipAddress;
                     myPrinterInfo.macAddress    = macAddress;
                         Log.d(TAG, "---- tejgo la data ----");
